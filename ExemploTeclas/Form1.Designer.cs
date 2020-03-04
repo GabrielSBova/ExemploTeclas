9ac4630e1b199c9e8526db4ad60b26a4bc9e81a5
Gabriel Bova <gabrielbova101@gmail.com>
2020-03-04 19:22:34 -0300

    Initial commit (aplicativo incompleto)

---
+++ ExemploTeclas/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace ExemploTeclas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.galinha = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.galinha)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // galinha
+            // 
+            this.galinha.Image = global::ExemploTeclas.Properties.Resources.chicken;
+            this.galinha.Location = new System.Drawing.Point(224, 162);
+            this.galinha.Name = "galinha";
+            this.galinha.Size = new System.Drawing.Size(393, 251);
+            this.galinha.TabIndex = 0;
+            this.galinha.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.galinha);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.galinha)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox galinha;
+    }
+}
+
